ac90ac9276f7b438946b7dc04e8af88556160db8
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-11 17:05:52 +0300

    Create base character

---
+++ Source/ZombieShooter/Public/BaseCharacter.h
@@ -0,0 +1,25 @@
+// Created by Artyom Volkov during the UE4 course
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS()
+class ZOMBIESHOOTER_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};

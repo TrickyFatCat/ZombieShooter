922b6b583d6d0e887ef99f31c8fa54d2d45ae009
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-12 20:14:12 +0300

    Implement getter functions into WeaponBase

--- Source/ZombieShooter/Public/Weapons/WeaponBase.h
+++ Source/ZombieShooter/Public/Weapons/WeaponBase.h
@@ -23,6 +23,12 @@ protected:
 public:
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION(BlueprintCallable, Category="Weapon")
+	EWeaponType GetWeaponType() const { return WeaponTypeData.WeaponType; }
+
+	UFUNCTION(BlueprintCallable, Category="Weapon")
+	FName GetWeaponSocketName() const { return WeaponTypeData.WeaponSocketName; }
+
 private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components", meta=(AllowPrivateAccess="true"))
 	USceneComponent* WeaponRoot = nullptr;

5582a8c45caa02494484e86a29b2a7d4ac8833ec
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-12 19:43:18 +0300

    Create additional weapon structs

--- Source/ZombieShooter/Public/Weapons/WeaponCoreTypes.h
+++ Source/ZombieShooter/Public/Weapons/WeaponCoreTypes.h
@@ -4,6 +4,8 @@
 
 #include "WeaponCoreTypes.generated.h"
 
+class AWeaponBase;
+
 UENUM(BlueprintType)
 enum class EWeaponType : uint8
 {
@@ -15,3 +17,26 @@ enum class EWeaponType : uint8
 	SniperRifle
 };
 
+USTRUCT(BlueprintType)
+struct FWeaponSpawnData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, Category="Weapon")
+	TSubclassOf<AWeaponBase> WeaponClass = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="Weapon")
+	UTexture2D* WeaponIcon = nullptr;
+};
+
+USTRUCT(BlueprintType)
+struct FWeaponTypeData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, Category="Weapon")
+	EWeaponType WeaponType = EWeaponType::Pistol;
+
+	UPROPERTY(EditAnywhere, Category="Weapon")
+	FName WeaponSocketName = NAME_None;
+};

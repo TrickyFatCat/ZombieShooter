d78f4acf9d6d4b4c1b7a41058712ffcffd2a96fd
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-12 23:07:41 +0300

    Update weapon component 1. Add weapon spawn on begin play; 2. Implement simple weapon switch functionality;

--- Source/ZombieShooter/Private/Components/WeaponComponent.cpp
+++ Source/ZombieShooter/Private/Components/WeaponComponent.cpp
@@ -3,6 +3,9 @@
 
 #include "Components/WeaponComponent.h"
 
+#include "Characters/PlayerCharacter.h"
+#include "Weapons/WeaponBase.h"
+
 UWeaponComponent::UWeaponComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
@@ -12,11 +15,86 @@ UWeaponComponent::UWeaponComponent()
 void UWeaponComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SpawnWeapons();
+	EquipWeapon(CurrentWeaponIndex);
 }
 
 
-void UWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UWeaponComponent::TickComponent(float DeltaTime,
+                                     ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
+void UWeaponComponent::SpawnWeapons()
+{
+	if (!GetWorld() || !GetOwner()) return;
+
+	APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(GetOwner());
+
+	if (!PlayerCharacter) return;
+
+	for (auto WeaponClass : WeaponClasses)
+	{
+		AWeaponBase* Weapon = GetWorld()->SpawnActor<AWeaponBase>(WeaponClass);
+
+		if (!Weapon) continue;
+
+		// Add delegate for auto reload
+		Weapon->SetOwner(GetOwner());
+		Weapons.Add(Weapon);
+		FAttachmentTransformRules AttachmentTransformRules(EAttachmentRule::SnapToTarget, false);
+		Weapon->AttachToComponent(PlayerCharacter->GetPlayerArms(), AttachmentTransformRules, Weapon->GetWeaponSocketName());
+		Weapon->SetActorHiddenInGame(true);
+	}
+}
+
+void UWeaponComponent::EquipNextWeapon()
+{
+	if (bIsEquipping && bIsReloading) return;
+
+	CurrentWeaponIndex = (CurrentWeaponIndex + 1) % Weapons.Num();
+	bIsEquipping = true;
+	EquipWeapon(CurrentWeaponIndex);
+	bIsEquipping = false;
+	// Play equip animation
+}
+
+void UWeaponComponent::EquipPreviousWeapon()
+{
+	if (bIsEquipping && bIsReloading) return;
+
+	CurrentWeaponIndex = CurrentWeaponIndex == 0 ? Weapons.Num() - 1 : CurrentWeaponIndex - 1;
+	bIsEquipping = true;
+	EquipWeapon(CurrentWeaponIndex);
+	bIsEquipping = false;
+	// Play equip animation
+}
+
+void UWeaponComponent::StartShooting()
+{
+}
+
+void UWeaponComponent::StopShooting()
+{
+}
+
+void UWeaponComponent::Reload()
+{
+}
+
+void UWeaponComponent::EquipWeapon(const int32 WeaponIndex)
+{
+	if (!GetOwner() || WeaponIndex >= Weapons.Num() || Weapons.Num() == 0) return;
+
+	if (CurrentWeapon)
+	{
+		// CurrentWeapon->StopShooting();
+		CurrentWeapon->SetActorHiddenInGame(true);
+	}
+
+	CurrentWeapon = Weapons[WeaponIndex];
+	CurrentWeapon->SetActorHiddenInGame(false);
+}
--- Source/ZombieShooter/Public/Components/WeaponComponent.h
+++ Source/ZombieShooter/Public/Components/WeaponComponent.h
@@ -4,28 +4,72 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Weapons/WeaponCoreTypes.h"
 #include "WeaponComponent.generated.h"
 
 class AWeaponBase;
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class ZOMBIESHOOTER_API UWeaponComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	UWeaponComponent();
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:
+	virtual void TickComponent(float DeltaTime,
+	                           ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintGetter, Category="Weapon")
 	AWeaponBase* GetCurrentWeapon() const { return CurrentWeapon; }
+
 private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapons", meta=(AllowPrivateAccess="true"))
+	TArray<TSubclassOf<AWeaponBase>> WeaponClasses;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Weapons", meta=(AllowPrivateAccess="true"))
+	TArray<AWeaponBase*> Weapons;
+	
 	UPROPERTY(BlueprintGetter=GetCurrentWeapon)
 	AWeaponBase* CurrentWeapon = nullptr;
+
+	int32 CurrentWeaponIndex = 0;
+
+	void SpawnWeapons();
+
+	// Weapon control
+public:
+	void EquipNextWeapon();
+	void EquipPreviousWeapon();
+	void StartShooting();
+	void StopShooting();
+	void Reload();
+protected:
+	void EquipWeapon(const int32 WeaponIndex);
+
+	// Weapon parameters
+public:
+	UFUNCTION(BlueprintCallable, Category="Weapon")
+	bool CanFire() const { return !bIsReloading && !bIsEquipping; }
+	
+	UFUNCTION(BlueprintGetter, Category="Weapon")
+	bool GetIsReloading() const { return bIsReloading; }
+
+	UFUNCTION(BlueprintGetter, Category="Weapon")
+	bool GetIsEquipping() const { return bIsEquipping; }
+
+protected:
+	
+private:
+	UPROPERTY(BlueprintGetter=GetIsReloading, Category="Weapon")
+	bool bIsReloading = false;
+
+	UPROPERTY(BlueprintGetter=GetIsEquipping, Category="Weapon")
+	bool bIsEquipping = false;
 };

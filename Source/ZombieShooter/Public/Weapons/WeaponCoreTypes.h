75014794bec18e2be458204ba567ba8b26eb1464
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-13 19:41:07 +0300

    Update weapon core types 1. Add FWeaponData struct; 2. Add FWeaponAmmoData struct; 3. Add EBulletType enum;

--- Source/ZombieShooter/Public/Weapons/WeaponCoreTypes.h
+++ Source/ZombieShooter/Public/Weapons/WeaponCoreTypes.h
@@ -5,6 +5,7 @@
 #include "WeaponCoreTypes.generated.h"
 
 class AWeaponBase;
+class AProjectileBase;
 
 UENUM(BlueprintType)
 enum class EWeaponType : uint8
@@ -15,6 +16,13 @@ enum class EWeaponType : uint8
 	GrenadeLauncher,
 	RocketLauncher,
 	SniperRifle
+};
+
+UENUM(BlueprintType)
+enum class EBulletType : uint8
+{
+	HitScan,
+	Projectile
 };
 
 USTRUCT(BlueprintType)
@@ -30,13 +38,58 @@ struct FWeaponSpawnData
 };
 
 USTRUCT(BlueprintType)
-struct FWeaponTypeData
+struct FWeaponData
 {
 	GENERATED_USTRUCT_BODY()
 
-	UPROPERTY(EditAnywhere, Category="Weapon")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
 	EWeaponType WeaponType = EWeaponType::Pistol;
 
-	UPROPERTY(EditAnywhere, Category="Weapon")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
 	FName WeaponSocketName = NAME_None;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon")
+	int32 Damage = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon")
+	float RateOfFire = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon")
+	float Spread = 5.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon", meta=(ClampMin="0"))
+	int32 ShotCost = 1; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon")
+	EBulletType BulletType = EBulletType::HitScan;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon", meta=(EditCondition="BulletType==EBulletType::Projectile"))
+	TSubclassOf<AProjectileBase> ProjectileClass = nullptr;
+};
+
+USTRUCT(BlueprintType)
+struct FWeaponAmmoData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ammo")
+	EWeaponType AmmoType = EWeaponType::Pistol;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ammo")
+	bool bIsClipInfinite = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Ammo")
+	int32 ClipAmmoCurrent = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ammo")
+	int32 ClipAmmoMax = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ammo")
+	bool bIsStorageInfinite = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Ammo")
+	int32 StorageAmmoCurrent = 300;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ammo")
+	int32 StorageAmmoMax = 300;
 };

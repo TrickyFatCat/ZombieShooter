144b84d1b01525a2fc17f70604067232bfed3313
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-17 21:32:51 +0300

    Create utility library

---
+++ Source/ZombieShooter/Private/Core/ProjectUtils.cpp
@@ -0,0 +1,24 @@
+﻿// Created by Artyom Volkov during the UE4 course
+
+#pragma once
+
+#include "Core/ProjectUtils.h"
+#include "GameFramework/Character.h"
+
+bool FProjectUtils::GetPlayerViewPoint(AActor* CharacterActor, FVector& ViewLocation, FRotator& ViewRotation)
+{
+	ACharacter* Character = Cast<ACharacter>(CharacterActor);
+
+	if (!Character) return false;
+
+	if (Character->IsPlayerControlled())
+	{
+		const APlayerController* Controller = Character->GetController<APlayerController>();
+
+		if (!Controller) return false;
+
+		Controller->GetPlayerViewPoint(ViewLocation, ViewRotation);
+	}
+
+	return true;
+}
---
+++ Source/ZombieShooter/Public/Core/ProjectUtils.h
@@ -0,0 +1,10 @@
+﻿// Created by Artyom Volkov during the UE4 course
+
+#pragma once
+
+
+class FProjectUtils
+{
+public:
+	static bool  GetPlayerViewPoint(AActor* CharacterActor, FVector& ViewLocation, FRotator& ViewRotation);
+};

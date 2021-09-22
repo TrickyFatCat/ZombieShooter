9f65067c41244267f1b0d8834b22687e0d205b23
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-22 23:46:54 +0300

    Implement basic death logic for zombie

--- Source/ZombieShooter/Private/Characters/EnemyCharacter.cpp
+++ Source/ZombieShooter/Private/Characters/EnemyCharacter.cpp
@@ -3,3 +3,19 @@
 
 #include "Characters/EnemyCharacter.h"
 
+void AEnemyCharacter::OnDeath(AController* DeathInstigator, AActor* DeathCauser, const UDamageType* DamageType)
+{
+	Super::OnDeath(DeathInstigator, DeathCauser, DamageType);
+
+	if (BaseExplosionClass && DamageType->IsA(BaseExplosionClass))
+	{
+		GetMesh()->SetSimulatePhysics(true);
+		return;
+	}
+
+	const float DotProduct = FVector::DotProduct(GetActorForwardVector(),
+	                                             DeathInstigator->GetPawn()->GetActorForwardVector());
+	const FName StartSectionName = DotProduct > 0.f ? DeathAnimMontageSections[0] : DeathAnimMontageSections[1];
+	PlayAnimMontage(DeathAnimMontage, 1.f, StartSectionName);
+	SetLifeSpan(DefaultLifeSpan);
+}
--- Source/ZombieShooter/Public/Characters/EnemyCharacter.h
+++ Source/ZombieShooter/Public/Characters/EnemyCharacter.h
@@ -6,6 +6,8 @@
 #include "Characters/BaseCharacter.h"
 #include "EnemyCharacter.generated.h"
 
+class UAnimMontage;
+
 /**
  * 
  */
@@ -13,5 +15,20 @@ UCLASS()
 class ZOMBIESHOOTER_API AEnemyCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
-	
+
+	// Death
+protected:
+	virtual void OnDeath(AController* DeathInstigator, AActor* DeathCauser, const UDamageType* DamageType) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Enemy|Animations")
+	UAnimMontage* DeathAnimMontage = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Enemy|Animations")
+	TArray<FName> DeathAnimMontageSections = {FName("Death_Front"), FName("Death_Back")};
+
+	UPROPERTY(EditDefaultsOnly, Category="Enemy|Animations")
+	TSubclassOf<UDamageType> BaseExplosionClass = nullptr;
+
+	const float DefaultLifeSpan = 5.f;
 };

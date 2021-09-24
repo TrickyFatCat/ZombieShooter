ce5142741522932889bd31a5bbc9256845ac22b7
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-24 18:09:06 +0300

    Create EnemyMovementData struct

---
+++ Source/ZombieShooter/Public/Characters/EnemyCoreTypes.h
@@ -0,0 +1,23 @@
+﻿// Created by Artyom Volkov during the UE4 course
+
+#pragma once
+
+#include "EnemyCoreTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEnemyMovementData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy", meta=(ClampMin="0"))
+	float WalkingSpeedDefault = 200.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy", meta=(ClampMin="0"))
+	float WalkingSpeedRandomDeviation = 0.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy", meta=(ClampMin="0"))
+	float RunningSpedDefault = 600.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy", meta=(ClampMin="0"))
+	float RunningSpeedRandomDeviation = 0.f;
+};

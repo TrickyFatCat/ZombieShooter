dc7e4f834773ef514f11c944940164b18a7fe001
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-27 18:20:05 +0300

    Create base enemy spawn

---
+++ Source/ZombieShooter/Private/Actors/BaseEnemySpawn.cpp
@@ -0,0 +1,114 @@
+// Created by Artyom Volkov during the UE4 course
+
+
+#include "Actors/BaseEnemySpawn.h"
+
+#include "Characters/EnemyCharacter.h"
+#include "Math/TransformCalculus3D.h"
+
+ABaseEnemySpawn::ABaseEnemySpawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABaseEnemySpawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CachedSpawnTable = SpawnTable;
+
+	for (const auto SpawnData : CachedSpawnTable)
+	{
+		if (!SpawnData.EnemyClass) continue;
+
+		SpawnedEnemiesMax += SpawnData.Number;
+	}
+}
+
+void ABaseEnemySpawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABaseEnemySpawn::StartSpawn()
+{
+	if (SpawnTable.Num() <= 0 || SpawnedEnemiesMax <= 0) return;
+
+	ProcessSpawnQueue();
+
+	GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &ABaseEnemySpawn::ProcessSpawnQueue, SpawnPause, true);
+}
+
+void ABaseEnemySpawn::StopSpawn()
+{
+	if (SpawnTable.Num() <= 0 || !GetWorldTimerManager().IsTimerActive(SpawnTimerHandle)) return;
+
+	GetWorldTimerManager().ClearTimer(SpawnTimerHandle);
+}
+
+AEnemyCharacter* ABaseEnemySpawn::SpawnEnemy(const TSubclassOf<AEnemyCharacter> EnemyClass,
+                                             const FTransform& SpawnTransform)
+{
+	AEnemyCharacter* EnemyCharacter = GetWorld()->SpawnActor<AEnemyCharacter>(EnemyClass, SpawnTransform);
+
+	if (!EnemyCharacter) return nullptr;
+
+	OnEnemySpawned.Broadcast(EnemyCharacter);
+
+	return EnemyCharacter;
+}
+
+void ABaseEnemySpawn::ProcessSpawnQueue()
+{
+	if (CurrentEnemyID >= CachedSpawnTable.Num())
+	{
+		StopSpawn();
+		return;
+	}
+
+	for (int32 i = 1; i <= SpawnNumber; ++i)
+	{
+		if (CachedSpawnTable[CurrentEnemyID].Number <= 0 || CachedSpawnTable[CurrentEnemyID].EnemyClass == nullptr)
+		{
+			CurrentEnemyID++;
+
+			if (IsEnemyIdOutOfBounds()) break;
+
+			continue;
+		}
+
+		AEnemyCharacter* SpawnedEnemy = SpawnEnemy(SpawnTable[CurrentEnemyID].EnemyClass, CalculateSpawnTransform());
+
+		if (!SpawnedEnemy)
+		{
+			CurrentEnemyID++;
+
+			if (IsEnemyIdOutOfBounds()) break;
+
+			continue;
+		}
+
+		CachedSpawnTable[CurrentEnemyID].Number--;
+		SpawnedEnemiesCount++;
+
+		if (CachedSpawnTable[CurrentEnemyID].Number <= 0) CurrentEnemyID++;
+
+		if (IsEnemyIdOutOfBounds()) break;
+	}
+}
+
+FTransform ABaseEnemySpawn::CalculateSpawnTransform()
+{
+	return FTransform(FRotator::ZeroRotator, FVector::ZeroVector, FVector::OneVector);
+}
+
+bool ABaseEnemySpawn::IsEnemyIdOutOfBounds()
+{
+	if (CurrentEnemyID >= CachedSpawnTable.Num())
+	{
+		StopSpawn();
+		return true;
+	}
+
+	return false;
+}
---
+++ Source/ZombieShooter/Public/Actors/BaseEnemySpawn.h
@@ -0,0 +1,77 @@
+// Created by Artyom Volkov during the UE4 course
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseEnemySpawn.generated.h"
+
+class AEnemyCharacter;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEnemySpawnedSignature, AEnemyCharacter*, SpawnedCharacter);
+
+USTRUCT(BlueprintType)
+struct FSpawnData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn")
+	TSubclassOf<AEnemyCharacter> EnemyClass = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn", meta=(ClampMin="0"))
+	int32 Number = 1;
+};
+
+UCLASS()
+class ZOMBIESHOOTER_API ABaseEnemySpawn : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABaseEnemySpawn();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(BlueprintAssignable, Category="Spawn")
+	FOnEnemySpawnedSignature OnEnemySpawned;
+
+	UFUNCTION(BlueprintCallable, Category="Spawn")
+	void StartSpawn();
+
+	UFUNCTION(BlueprintCallable, Category="Spawn")
+	void StopSpawn();
+
+protected:
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Spawn")
+	TArray<FSpawnData> SpawnTable;
+
+	UPROPERTY(VisibleAnywhere, Category="Spawn")
+	TArray<FSpawnData> CachedSpawnTable;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Spawn", meta=(ClampMin="0"))
+	float SpawnPause = 1.f;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Spawn", meta=(ClampMin="1"))
+	int32 SpawnNumber = 1;
+
+	int32 CurrentEnemyID = 0;
+
+	int32 SpawnedEnemiesCount = 0;
+
+	int32 SpawnedEnemiesMax = 0;
+
+	FTimerHandle SpawnTimerHandle;
+
+	AEnemyCharacter* SpawnEnemy(const TSubclassOf<AEnemyCharacter> EnemyClass,
+	                            const FTransform& SpawnTransform);
+
+	UFUNCTION()
+	void ProcessSpawnQueue();
+
+	virtual FTransform CalculateSpawnTransform();
+
+	bool IsEnemyIdOutOfBounds();
+};

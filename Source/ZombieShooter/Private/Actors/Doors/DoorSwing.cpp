a5c08910776822f96a3eee66b8b2b7ac1597228c
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-29 16:02:39 +0300

    Implement swing door

---
+++ Source/ZombieShooter/Private/Actors/Doors/DoorSwing.cpp
@@ -0,0 +1,126 @@
+// Created by Artyom Volkov during the UE4 course
+
+
+#include "Actors/Doors/DoorSwing.h"
+#include "Components/TriggerComponents/InteractionBoxComponent.h"
+
+ADoorSwing::ADoorSwing()
+{
+	InteractionTrigger = CreateDefaultSubobject<UInteractionBoxComponent>("InteractionTrigger");
+	SetRootComponent(InteractionTrigger);
+}
+
+
+void ADoorSwing::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InteractionTrigger->SetIsNormalTrigger(!bRequireInteraction);
+	InteractionTrigger->bRequireLineOfSight = bRequireInteraction;
+
+	if (!bRequireInteraction)
+	{
+		InteractionTrigger->OnComponentBeginOverlap.AddDynamic(this, &ADoorSwing::OnTriggerBeginOverlap);
+		InteractionTrigger->OnComponentEndOverlap.AddDynamic(this, &ADoorSwing::OnTriggerEndOverlap);
+	}
+}
+
+void ADoorSwing::Disable()
+{
+	Super::Disable();
+
+	InteractionTrigger->SetIsEnabled(false);
+}
+
+void ADoorSwing::Enable()
+{
+	Super::Enable();
+
+	InteractionTrigger->SetIsEnabled(true);
+}
+
+bool ADoorSwing::ProcessInteraction_Implementation(APlayerController* PlayerController)
+{
+	if (!PlayerController || !bRequireInteraction) return false;
+
+	if (!GetIsReversible() && GetStateCurrent() == EInteractiveActorState::Transition) return false;
+
+	const AActor* PlayerActor = Cast<AActor>(PlayerController->GetPawn());
+
+	if (!PlayerActor) return false;
+
+	if (GetWorldTimerManager().IsTimerActive(AutoCloseDelayHandle))
+	{
+		GetWorldTimerManager().ClearTimer(AutoCloseDelayHandle);
+	}
+
+	CalculateTargetTransform(PlayerActor);
+	StartAnimation();
+	return true;
+}
+
+void ADoorSwing::FinishAnimation()
+{
+	Super::FinishAnimation();
+
+	if (AutoCloseDelay <= 0.f || InteractionTrigger->GetIsActorInside()) return;
+
+	GetWorldTimerManager().SetTimer(AutoCloseDelayHandle, this, &ADoorSwing::Close, AutoCloseDelay, false);
+}
+
+void ADoorSwing::OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                       AActor* OtherActor,
+                                       UPrimitiveComponent* OtherComp,
+                                       int32 OtherBodyIndex,
+                                       bool bFromSweep,
+                                       const FHitResult& SweepResult)
+{
+	CalculateTargetTransform(OtherActor);
+
+	if (AutoCloseDelay > 0.f)
+	{
+		GetWorldTimerManager().ClearTimer(AutoCloseDelayHandle);
+		return;
+	}
+
+	if (bRequireInteraction || GetStateCurrent() != EInteractiveActorState::Closed) return;
+
+	Open();
+}
+
+void ADoorSwing::OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent,
+                                     AActor* OtherActor,
+                                     UPrimitiveComponent* OtherComp,
+                                     int32 OtherBodyIndex)
+{
+	if (AutoCloseDelay > 0.f)
+	{
+		GetWorldTimerManager().SetTimer(AutoCloseDelayHandle, this, &ADoorSwing::Close, AutoCloseDelay, false);
+		return;
+	}
+	
+	if (bRequireInteraction || GetStateCurrent() != EInteractiveActorState::Opened) return;
+
+	Close();
+}
+
+void ADoorSwing::CalculateTargetTransform(const AActor* Actor)
+{
+	PrevSwingDirection = SwingDirection;
+	const float DotProduct = FVector::DotProduct(GetActorForwardVector(),
+	                                             (GetActorLocation() - Actor->GetActorLocation()).GetSafeNormal());
+	SwingDirection = FMath::Sign(DotProduct);
+
+	if (PrevSwingDirection != SwingDirection) return;
+
+	const float CurrentYaw = TargetTransforms[0].TargetRotation.Yaw;
+
+	if (SwingDirection < 0.f && FMath::Sign(CurrentYaw) < 0.f)
+	{
+		TargetTransforms[0].TargetRotation.Yaw *= SwingDirection;
+	}
+	else if (SwingDirection > 0.f && FMath::Sign(CurrentYaw) > 0.f)
+	{
+		TargetTransforms[0].TargetRotation.Yaw *= -SwingDirection;
+	}
+}
---
+++ Source/ZombieShooter/Public/Actors/Doors/DoorSwing.h
@@ -0,0 +1,65 @@
+// Created by Artyom Volkov during the UE4 course
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/InteractiveActors/DoorBase.h"
+#include "Interfaces/InteractionInterface.h"
+#include "DoorSwing.generated.h"
+
+class UInteractionBoxComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIESHOOTER_API ADoorSwing : public ADoorBase, public IInteractionInterface
+{
+	GENERATED_BODY()
+
+public:
+	ADoorSwing();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
+	UInteractionBoxComponent* InteractionTrigger = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Door")
+	bool bRequireInteraction = false;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Door", meta=(ClampMint="0"))
+	float AutoCloseDelay = 2.f;
+
+	float SwingDirection = 1.f;
+	
+	float PrevSwingDirection = 1.f;
+
+	FTimerHandle AutoCloseDelayHandle;
+
+	virtual void Disable() override;
+
+	virtual void Enable() override;
+
+	virtual bool ProcessInteraction_Implementation(APlayerController* PlayerController) override;
+
+	virtual void FinishAnimation() override;
+
+	UFUNCTION()
+	void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                           AActor* OtherActor,
+	                           UPrimitiveComponent* OtherComp,
+	                           int32 OtherBodyIndex,
+	                           bool bFromSweep,
+	                           const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent,
+	                         AActor* OtherActor,
+	                         UPrimitiveComponent* OtherComp,
+	                         int32 OtherBodyIndex);
+
+	void CalculateTargetTransform(const AActor* Actor);
+};

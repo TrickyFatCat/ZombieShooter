b078c3a3d6f4e9c780bda05550eff072498ce21d
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-30 19:25:02 +0300

    Create damage sphere component

---
+++ Source/ZombieShooter/Private/Components/TriggerComponents/DamageSphereComponent.cpp
@@ -0,0 +1,44 @@
+// Created by Artyom Volkov during the UE4 course
+
+
+#include "Components/TriggerComponents/DamageSphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void UDamageSphereComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UDamageSphereComponent::SetDamage(const int32 Value)
+{
+	if (Value < 0) return;
+
+	Damage = Value;
+}
+
+void UDamageSphereComponent::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                            AActor* OtherActor,
+                                            UPrimitiveComponent* OtherComp,
+                                            int32 OtherBodyIndex,
+                                            bool bFromSweep,
+                                            const FHitResult& SweepResult)
+{
+	Super::OnBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	UE_LOG(LogTemp, Error, TEXT("Overlapped Actor %s"), *OtherActor->GetName());
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	AController* OwnerController = nullptr;
+
+	if (OwnerPawn)
+	{
+		OwnerController = OwnerPawn->GetController();
+	}
+
+	UGameplayStatics::ApplyPointDamage(OtherActor,
+	                                   Damage,
+	                                   GetOwner()->GetActorLocation(),
+	                                   SweepResult,
+	                                   OwnerController,
+	                                   GetOwner(),
+	                                   DamageTypeClass);
+}

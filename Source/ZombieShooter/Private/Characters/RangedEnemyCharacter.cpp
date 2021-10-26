47583745a5e196dd9641f87810eba3c24dce2659
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-27 01:08:19 +0300

    Create new enemy class

---
+++ Source/ZombieShooter/Private/Characters/RangedEnemyCharacter.cpp
@@ -0,0 +1,78 @@
+// Created by Artyom Volkov during the UE4 course
+
+
+#include "Characters/RangedEnemyCharacter.h"
+
+#include "AIController.h"
+#include "Weapons/ProjectileBase.h"
+
+void ARangedEnemyCharacter::StartAttack(const EEnemyMeleeAttackType Attack)
+{
+	SpawnProjectiles();
+}
+
+void ARangedEnemyCharacter::FinishAttack()
+{
+}
+
+void ARangedEnemyCharacter::SpawnProjectiles()
+{
+	if (!GetWorld() || !SpitProjectile) return;
+
+	FVector TraceStart, TraceEnd;
+	FRotator ViewRotation;
+	FHitResult HitResult;
+
+	AAIController* AIController = Cast<AAIController>(GetController());
+
+	if (!Controller)
+	{
+		FinishAttack();
+		return;
+	}
+
+	Controller->GetActorEyesViewPoint(TraceStart, ViewRotation);
+	FVector AttackDirection = ViewRotation.Vector();
+	int32 ProjectileDamage = FMath::Max(AttackDamage / ProjectilesAmount, 1);
+
+	for (int32 i = 1; i <= ProjectilesAmount; ++i)
+	{
+		if (AttackSpread > 0.f)
+		{
+			const float SpreadAngleRad = FMath::DegreesToRadians(AttackSpread / 2);
+			AttackDirection = FMath::VRandCone(AttackDirection, SpreadAngleRad);
+		}
+
+		TraceEnd = TraceStart + AttackDirection * TraceDistance;
+
+		FCollisionObjectQueryParams CollisionObjectQueryParams = FCollisionObjectQueryParams(
+			ECollisionChannel::ECC_GameTraceChannel2);
+		FCollisionQueryParams CollisionQueryParams;
+		CollisionQueryParams.AddIgnoredActor(this);
+
+		GetWorld()->LineTraceSingleByObjectType(HitResult,
+		                                        TraceStart,
+		                                        TraceEnd,
+		                                        CollisionObjectQueryParams,
+		                                        CollisionQueryParams);
+
+		TraceEnd = HitResult.bBlockingHit ? HitResult.ImpactPoint : TraceEnd;
+		FVector Direction = (TraceEnd - TraceStart).GetSafeNormal();
+
+		if (!SpitProjectile) break;
+
+		const FTransform SpawnTransform(FRotator::ZeroRotator, TraceStart);
+		AProjectileBase* Projectile = GetWorld()->SpawnActorDeferred<AProjectileBase>(
+			SpitProjectile,
+			SpawnTransform);
+
+		if (Projectile)
+		{
+			Projectile->SetDirectionAndDamage(Direction, nullptr, ProjectileDamage);
+			Projectile->SetOwner(GetOwner());
+			Projectile->FinishSpawning(SpawnTransform);
+		}
+	}
+
+	
+}
---
+++ Source/ZombieShooter/Public/Characters/RangedEnemyCharacter.h
@@ -0,0 +1,46 @@
+// Created by Artyom Volkov during the UE4 course
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/EnemyCharacter.h"
+#include "RangedEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIESHOOTER_API ARangedEnemyCharacter : public AEnemyCharacter
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void StartAttack(const EEnemyMeleeAttackType AttackType) override;
+
+	virtual void FinishAttack() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Enemy|RangedAttack", meta=(AllowPrivateAccess="true"))
+	TSubclassOf<AProjectileBase> SpitProjectile = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Enemy|RangedAttack", meta=(AllowPrivateAccess="true"))
+	int32 ProjectilesAmount = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Enemy|RangedAttack", meta=(AllowPrivateAccess="true"))
+	float RateOfFire = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Enemy|RangedAttack", meta=(AllowPrivateAccess="true"))
+	int32 MaxAttackNumber = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Enemy|RangedAttack", meta=(AllowPrivateAccess="true"))
+	float AttackSpread = 5.f;
+
+	const float TraceDistance = 10000;
+
+	int32 CurrentAttackNumber = 0;
+
+	FTimerHandle AttackTimerHandle;
+
+	UFUNCTION()
+	void SpawnProjectiles();
+};

0009231aaa8031cbe8ba1d9809daa0791f629536
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-11-03 20:31:19 +0300

    Create service for checking line of sight

---
+++ Source/ZombieShooter/Private/AI/Services/BTService_CheckLineOfSight.cpp
@@ -0,0 +1,47 @@
+// Created by Artyom Volkov during the UE4 course
+
+
+#include "AI/Services/BTService_CheckLineOfSight.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
+
+UBTService_CheckLineOfSight::UBTService_CheckLineOfSight()
+{
+	NodeName = "Is In Line Of Sight";
+}
+
+void UBTService_CheckLineOfSight::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	bool bIsInLineOfSight = false;
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+	AActor* Owner = OwnerComp.GetOwner();
+	UWorld* World = GetWorld();
+
+	if (Blackboard && Owner && World)
+	{
+		AActor* TargetActor = Cast<AActor>(Blackboard->GetValueAsObject(TargetActorKey.SelectedKeyName));
+
+		if (TargetActor)
+		{
+			FHitResult HitResult;
+			FCollisionQueryParams CollisionQueryParams;
+			CollisionQueryParams.AddIgnoredActor(Owner);
+			FCollisionResponseParams CollisionResponseParams;
+
+			UKismetSystemLibrary::SphereTraceSingle(World,
+			                                        Owner->GetActorLocation(),
+			                                        TargetActor->GetActorLocation(),
+			                                        64,
+			                                        UEngineTypes::ConvertToTraceType(ECC_GameTraceChannel1),
+			                                        false,
+			                                        {Owner},
+			                                        EDrawDebugTrace::None,
+			                                        HitResult,
+			                                        true);
+			bIsInLineOfSight = !HitResult.bBlockingHit;
+		}
+	}
+	Blackboard->SetValueAsBool(IsInLineOfSightKey.SelectedKeyName, bIsInLineOfSight);
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
